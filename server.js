269dce87c60b6a5b9f925f47122cb55fb7790688
chris6661 <74381468+chris6661@users.noreply.github.com>
2022-08-09 21:56:28 -0500

    added index, model, server files:

--- server.js
+++ server.js
@@ -1,17 +1,69 @@
-const express = require("express"); 
+const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
-const merge = require("lodash/merge"); 
-const mongoose = require("mongoose"); 
-const { PubSub } = require("apollo-server"); 
-const { createServer } = require("http"); 
-require("dotenv").config(); 
+const merge = require("lodash/merge");
+const mongoose = require("mongoose");
+const { PubSub } = require("apollo-server");
+const { createServer } = require("http");
 
-const typeDefs = gql``; 
+require("dotenv").config();
+const { cardResolvers, cardTypeDefs } = require("./card");
+const { sectionResolvers, sectionTypeDefs } = require("./section");
 
-const resolvers = {}; 
+const cardModel = require("./card/model");
+const sectionModel = require("./section/model");
+const SUBSCRIPTION_CONSTANTS = require("./subscriptionConstants");
 
-const MONGO_USER = process.env.MONGO_USER || "root"; 
-const MONGO_PASS = process.env.MONGODB_PASS; 
+const typeDefs = gql`
+  type Subscription {
+    sectionAdded: Section
+    cardAdded: Card
+    onSectionPosChange: Section
+    onCardPosChange: Card
+  }
+  ${cardTypeDefs}
+  ${sectionTypeDefs}
+`;
+
+const pubsub = new PubSub();
+
+const SubscriptionsResolvers = {
+  Subscription: {
+    sectionAdded: {
+      subscribe: () =>
+        pubsub.asyncIterator([SUBSCRIPTION_CONSTANTS.SECTION_ADDED]),
+    },
+    cardAdded: {
+      subscribe: () =>
+        pubsub.asyncIterator([SUBSCRIPTION_CONSTANTS.CARD_ADDED]),
+    },
+    onSectionPosChange: {
+      subscribe: () =>
+        pubsub.asyncIterator([SUBSCRIPTION_CONSTANTS.ON_SECTION_POS_CHANGE]),
+    },
+    onCardPosChange: {
+      subscribe: () =>
+        pubsub.asyncIterator([SUBSCRIPTION_CONSTANTS.ON_CARD_POS_CHANGE]),
+    },
+  },
+};
+
+const customResolvers = {
+  Section: {
+    cards(parent, args, cxt) {
+      return cxt.card.getCardBySectionId(parent._id);
+    },
+  },
+};
+
+const resolvers = merge(
+  cardResolvers,
+  sectionResolvers,
+  customResolvers,
+  SubscriptionsResolvers
+);
+
+const MONGO_USER = process.env.MONGO_USER || "root";
+const MONGO_PASS = process.env.MONGODB_PASS;
 
 mongoose
   .connect(
@@ -20,14 +72,24 @@ mongoose
   )
   .then(() => {
     console.log("mongodb connected successfully");
+
     const server = new ApolloServer({
       typeDefs,
-      resolvers
+      resolvers,
+      context: () => ({
+        card: cardModel,
+        section: sectionModel,
+        publisher: pubsub,
+        SUBSCRIPTION_CONSTANTS: SUBSCRIPTION_CONSTANTS,
+      }),
     });
+
     const app = express();
     server.applyMiddleware({ app });
+
     const httpServer = createServer(app);
-    
+    server.installSubscriptionHandlers(httpServer);
+
     const PORT = process.env.PORT || 4444;
     httpServer.listen({ port: PORT }, () => {
       console.log(`Server is running in port ${PORT}`);
@@ -35,19 +97,4 @@ mongoose
   })
   .catch((err) => {
     console.log(err);
-  });
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: () => ({
-    }),
-  });
-  const app = express();
-  server.applyMiddleware({ app });
-  const httpServer = createServer(app);
-  server.installSubscriptionHandlers(httpServer);
-  const PORT = process.env.PORT || 4444;
-  httpServer.listen({ port: PORT }, () => {
-    console.log(`Server is running in port ${PORT}`);
   });
